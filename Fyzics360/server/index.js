6a175d6b08692e20b2f2f3643011284c87c65b76
Marino Demo <demo716@gmail.com>
2018-10-05 17:50:43 -0400

    Updated seed file

--- Fyzics360/server/index.js
+++ Fyzics360/server/index.js
@@ -3,18 +3,24 @@ const morgan = require('morgan')
 const bodyparser = require('body-parser')
 const app = express()
 const PORT = 3000
+const db = require('./db')
 
 app.use(morgan('dev'))
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: true }))
+
+app.use('api', require('./api'))
 
 app.use((err, res, next) => {
   console.error(err)
   console.error(err.stack)
   res.status(err.status || 500).send(err.message || 'Internal server error')
 })
+
 db.sync().then(() => console.log('Database is synced'))
+
 app.listen(PORT, () => {
   console.log('Feeling lucky')
 })
+
 module.exports = app;
